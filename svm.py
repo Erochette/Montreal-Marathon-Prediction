746c0770d01f445b7871db3ca3b7eefec2139da5
Megan <megan_4@live.ca>
2016-11-05 23:46:12 -0400

    adding knn and svm

--- svm.py
+++ svm.py
@@ -1,10 +1,7 @@
 from sklearn.decomposition import RandomizedPCA
 from sklearn.preprocessing import StandardScaler
-from sklearn.cross_validation import train_test_split
 from sklearn import svm
-from sklearn.neighbors import KNeighborsClassifier
-from sklearn.metrics import confusion_matrix
-
+from sklearn.cross_validation import train_test_split
 
 import os
 import sys
@@ -17,34 +14,30 @@ import theano
 
 def load_dataset():
     std_scaler = StandardScaler()
-    train_x = np.fromfile('train_x.bin',dtype='uint8')
-    train_x = train_x.reshape((100000,60*60))
-    test_x2=train_x[800001:99999]
-    
-    pca = RandomizedPCA(n_components=60)
-
+    train_y = np.genfromtxt('train_y.csv', delimiter=',', dtype='int32', skip_header=1)[:, 1]
+    X = np.fromfile('train_x.bin',dtype='uint8')
+    train_x = X.reshape((-1,60*60))
+    test_x2=X.reshape((-1,60*60))
     train_x=train_x[:80000]
+    test_x2=test_x2[80001:99999]
+    train_y = np.genfromtxt('train_y.csv', delimiter=',', dtype='int32', skip_header=1)[:, 1]
+
+    pca = RandomizedPCA(n_components=60)
+    
     train_x=pca.fit_transform(train_x)
     test_x2 = pca.transform(test_x2)
     train_x = std_scaler.fit_transform(train_x)
     test_x2 = std_scaler.transform(test_x2)
-    train_y = np.genfromtxt('train_y.csv', delimiter=',', dtype='int32', skip_header=1)[:, 1]
-    test_y2=train_y[80001:99999]
     train_y=train_y[:80000]
+    test_y2=train_y[800001:99999]
 
     return train_x,train_y,test_x2, test_y2
 print "loading data.."
 X, y,test_x2,test_y2= load_dataset()
 print "Data loaded"
-clf = KNeighborsClassifier(n_neighbors=500)
+clf = svm.SVC(decision_function_shape='ovo')
 clf.fit(X, y)
-print "done"
-print "="*20
-#print clf
-
-print "Confusion Matrix"
-print "="*40
-#print confusion_matrix(test_y2, clf.predict(test_x2))
+print test_y2
 res= clf.predict(test_x2)
 correct = 0
 total = 0
